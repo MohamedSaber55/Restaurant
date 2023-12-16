dc2e5b6cc8db36be9d63a9dccc1f6f361f1cd6fe
Mohamed Saber <mohamed.saber.7753@gmail.com>
2023-12-16 02:38:50 +0200

    commit 2

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 <h1>Foodie website</h1>
+<p>by <h1>Mohamed Saber</h1></p>
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -41,31 +41,34 @@ const Testimonials = () => {
         pauseOnFocus: true,
     };
     return (
-        <div data-aos="fade-up" data-aos-duration="300" className="py-10 bg-white dark:bg-gray-900 dark:text-white duration-200">
-            <div className="container">
-                {/* Header */}
-                <div className="text-center mb-20 max-w-[400px] mx-auto">
-                    <p className="text-sm bg-clip-text text-transparent bg-gradient-to-r from-primary to-second">Testimonials</p>
-                    <h2 className="text-3xl font-bold">Testimonials</h2>
-                    <p className="text-xs text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore eos deserunt iste. Numquam voluptatum in pariatur reprehenderit quasi hic asperiores.</p>
-                </div>
-                {/* Section */}
-                <div data-aos="zoom-in" data-aos-duration="300" className="grid grid-cols-1 max-w-[600px] mx-auto gap-6">
-                    <Slider {...settings}>
-                        {/* Item 1 */}
-                        {testimonialsData?.map(item => {
-                            return (
-                                <div key={item.id} className="my-6">
-                                    <div className="flex flex-col justify-center items-center gap-4 text-center shadow-lg p-4 mx-4 rounded-xl bg-primary/10 dark:bg-gray-800 relative">
-                                        <img src={item.img} className="rounded-full block mx-auto " alt="" />
-                                        <p className="text-gray-500 text-sm ">{item.text}</p>
-                                        <h5 className="text-xl font-bold">{item.name}</h5>
-                                        <p className="text-black/20 text-9xl font-serif absolute top-0 right-0">,,</p>
+
+        <div className="py-10 bg-white dark:bg-gray-900 dark:text-white duration-200">
+            <div data-aos="fade-up" data-aos-duration="300">
+                <div className="container">
+                    {/* Header */}
+                    <div className="text-center mb-20 max-w-[400px] mx-auto">
+                        <p className="text-sm bg-clip-text text-transparent bg-gradient-to-r from-primary to-second">Testimonials</p>
+                        <h2 className="text-3xl font-bold">Testimonials</h2>
+                        <p className="text-xs text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore eos deserunt iste. Numquam voluptatum in pariatur reprehenderit quasi hic asperiores.</p>
+                    </div>
+                    {/* Section */}
+                    <div data-aos="zoom-in" data-aos-duration="300" className="grid grid-cols-1 max-w-[600px] mx-auto gap-6">
+                        <Slider {...settings}>
+                            {/* Item 1 */}
+                            {testimonialsData?.map(item => {
+                                return (
+                                    <div key={item.id} className="my-6">
+                                        <div className="flex flex-col justify-center items-center gap-4 text-center shadow-lg p-4 mx-4 rounded-xl bg-primary/10 dark:bg-gray-800 relative">
+                                            <img src={item.img} className="rounded-full block mx-auto " alt="" />
+                                            <p className="text-gray-500 text-sm ">{item.text}</p>
+                                            <h5 className="text-xl font-bold">{item.name}</h5>
+                                            <p className="text-black/20 text-9xl font-serif absolute top-0 right-0">,,</p>
+                                        </div>
                                     </div>
-                                </div>
-                            )
-                        })}
-                    </Slider>
+                                )
+                            })}
+                        </Slider>
+                    </div>
                 </div>
             </div>
         </div>
